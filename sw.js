3824c06ba228f35169df28bc0bd6b93e2bbc6e7f
viptu1979-maker <viptu1979@gmail.com>
2026-08-18 11:16:10 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "hr-system-cache-v1";
+const CORE_ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+/* شبكة أولاً مع رجوع للكاش لو مفيش إنترنت (عشان بيانات Firebase تفضل حديثة أول ما يبقى فيه نت) */
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
