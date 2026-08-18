30286125394c3e7a4cd6877262b9c94bb22e8b4e
viptu1979-maker <viptu1979@gmail.com>
2026-08-18 11:28:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const CACHE_NAME = "hr-system-cache-v1";
 const CORE_ASSETS = [
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
